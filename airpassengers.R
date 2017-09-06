b78ab2c9d6abe14aff5b2f0119843080add13c2e
Anirudh Jayaraman <anirudhjayaraman@users.noreply.github.com>
2017-09-06 10:20:44 +0530

    Example Time Series Analysis

---
+++ airpassengers.R
@@ -0,0 +1,65 @@
+library(tseries)
+library(zoo)
+library(forecast)
+
+data("AirPassengers")
+dat <- AirPassengers
+
+# Is there seasonality in the data? Provide supportive analysis. --------------
+boxplot(dat ~ cycle(dat)) # indicates presence of seasonality
+plot(decompose(dat))
+
+# Order of integration required to make this series stationary? ---------------
+nsdiffs(dat) # number of seasonal differences required to make the sereis stationary
+ndiffs(dat) # number of first differences to make the series stationary
+
+# ...also:
+
+adf.test(dat) # rejects the NULL : Alternative hypothesis is of Stationarity
+pp.test(dat) # rejects the NULL: Alternative hypothesis is of Stationarity
+kpss.test(dat) # rejects the NULL : Alternative hypothesis is of Unit Root
+
+# hence series is non-stationary
+
+diff_dat <- diff(dat) # take the differenced series
+
+adf.test(diff_dat) # rejects the NULL : Alternative hypothesis is of Stationarity
+pp.test(diff_dat) # rejects the NULL : Alternative hypothesis is of Stationarity
+kpss.test(diff_dat) # fails to reject the NULL hypothesis of Stationarity
+
+# hence the differenced series is stationary
+
+
+# What is the value of p and q in the final model? ---------------------------
+
+# remove unequal variance
+ldat <- log(dat)
+plot(ldat) 
+
+# De-trend the data
+ldat_trend <- ma(ldat, order = 12, centre = T)
+lines(ldat_trend)
+
+# plot de-trended series; seasonal component still needs removing
+de_trended_ldat <- ldat - ldat_trend
+plot(de_trended_ldat)
+
+# Calculate seasonal component
+de_trended_ldat_matrix <- matrix(data = de_trended_ldat, nrow = 12, byrow = T)
+seasonal_component <- colMeans(de_trended_ldat_matrix, na.rm = T)
+seasonal_ts <- ts(rep(seasonal_component, 12), start = c(1949,1), 
+                  end = c(1960,12), frequency = 12)
+
+# De-seasonalize the data
+ts_final <- de_trended_ldat - seasonal_ts
+plot(ts_final)
+plot(ts_final)
+
+ts_final_testing <- window(ts_final, start = c(1949,7), end = c(1960,6))
+plot(ts_final_testing)
+adf.test(ts_final_testing)
+auto.arima(ts_final_testing)
+
+
+
+
